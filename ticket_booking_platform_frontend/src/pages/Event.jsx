2ea5642948c35dc6b73a4a5cd26f90dddc1cf18b
mmohashik <127235049+mmohashik@users.noreply.github.com>
2025-03-21 20:56:50 +0530

    event card included

--- ticket_booking_platform_frontend/src/pages/Event.jsx
+++ ticket_booking_platform_frontend/src/pages/Event.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import Navbar from '../components/NavBar'
-import Footer from '../components/Footer'
-import MiddleTextEvents from '../components/MiddleTextEvents'
-
-
+import React from "react";
+import Navbar from "../components/NavBar";
+import Footer from "../components/Footer";
+import MiddleTextEvents from "../components/MiddleTextEvents";
+import EventCard from "../components/EventCard";
 
 const Event = () => {
   return (
     <div>
-      <Navbar/>
-      <MiddleTextEvents/>
+      <Navbar />
+      <MiddleTextEvents />
 
-      <Footer/>
+      {/* Event Cards Grid */}
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+        </div>
+      </div>
+
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Event
+export default Event;
